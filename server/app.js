9b65520b0d85f7bbccad6c64632b25b5cfde8ba6
alexander1fein <feinberg.alexander@gmail.com>
2022-09-07 14:11:15 -0400

    Done

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 // Your code here
+require('dotenv').config()
+
 
 app.get('/', (req, res) => {
     res.send(process.env.SECRET_MESSAGE);
